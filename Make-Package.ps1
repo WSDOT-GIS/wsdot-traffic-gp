efa40cdb702286fe016ba13b8606745e09cab628
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2017-06-29 16:58:09 -0700

    Created wsdot package namespace

--- Make-Package.ps1
+++ Make-Package.ps1
@@ -3,7 +3,7 @@
     Creates the Python package files
 #>
 
-Write-Host "Creating source distribution..."
+Write-Host "Creating source distribution..." -ForegroundColor Green
 python setup.py sdist
-Write-Host "Creating Universal Wheel..."
+Write-Host "Creating Universal Wheel..." -ForegroundColor Green
 python setup.py bdist_wheel --universal
