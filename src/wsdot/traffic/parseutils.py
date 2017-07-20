f3923550cd700d808caebbe4e704b85527ba32a7
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2017-07-20 10:54:14 -0700

    Modified parsing of WCF datetime wire format
    parse_wcf_date function can now optionally output either UTC or local time.

--- src/wsdot/traffic/parseutils.py
+++ src/wsdot/traffic/parseutils.py
@@ -6,7 +6,7 @@ import datetime
 import time
 import re
 
-WCF_DATE_RE = re.compile(r"\/Date\((\d+)([+\-]\d+)\)\/", re.IGNORECASE)
+WCF_DATE_RE = re.compile(r"\/Date\((?P<ms_since_1970_1_1>\d+)(?P<utc_offset>(?P<offset_sign>[+\-])(?P<offset_hrs>\d{2})(?P<offset_min>\d{2}))\)\/", re.IGNORECASE)
 _CAMEL_CASE_RE = re.compile(r"(?:[A-Z][a-z]+)")
 
 # ==RRTs (Related Roadway Type)==
@@ -60,25 +60,44 @@ class SRFormatError(ValueError):
         return msg_fmt % self.value
 
 
-def parse_wcf_date(wcf_date, throw_on_wrong_format=False):
+def parse_wcf_date(wcf_date, throw_on_wrong_format=False, output_utc=True):
     """Parses a WCF serialized date to a date string.
     :param wcf_date: A date/time in WCF JSON serialized format.
     :type wcf_date: str
     :param throw_on_wrong_format: Set to True to throw error on wrong format,
         False (default) to simply return original string.
+    :param output_utc: Set to True (default) to output UTC time, false to include timezone info.
+        This value will be ignored if the input wcf_date string does not include timezone info.
+    :type output_utc: bool
     :rtype: datetime.datetime or str or unicode
+    :see: https://docs.microsoft.com/en-us/dotnet/framework/wcf/feature-details/stand-alone-json-serialization#datetime-wire-format
     """
     if not isinstance(wcf_date, str):
         raise TypeError("Only str and unicode types are supported.")
     match = WCF_DATE_RE.match(wcf_date)
     if match:
         groups = match.groups()
+
         ticks = None
-        if len(groups) >= 2:
-            ticks = (int(groups[0]) + int(groups[1])) / 1000
+        if not output_utc and len(groups) >= 2:
+            # Use timezone info. See https://docs.python.org/3/library/datetime.html#timezone-objects
+            groupdict = match.groupdict()
+            ticks = int(groupdict["ms_since_1970_1_1"]) / 1000
+            hrs = int(groupdict["offset_hrs"])
+            mins = int(groupdict["offset_min"])
+            if groupdict["offset_sign"] == "-":
+                sign = -1
+            else:
+                sign = 1
+            delta = datetime.timedelta(hours=hrs * sign, minutes=mins * sign)
+            time_zone = datetime.timezone(delta)
+            return datetime.datetime.fromtimestamp(ticks, time_zone)
         else:
+            # Discard timezone info and use UTC time.
             ticks = int(groups[0]) / 1000
-        return datetime.datetime.fromtimestamp(ticks)
+            return datetime.datetime.utcfromtimestamp(ticks)
+
+
     elif throw_on_wrong_format:
         raise ValueError("Could not parse as a WCF date string: %s." %
                          wcf_date)
