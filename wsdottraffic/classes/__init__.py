f58942ec39001c8c2dd769fefcd1b7402057466d
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2018-09-17 17:17:22 -0700

    Added __geo_interface__ properties

--- wsdottraffic/classes/__init__.py
+++ wsdottraffic/classes/__init__.py
@@ -1,11 +1,30 @@
 """Defines Python data classes for Traffic API
 """
+import copy
 import datetime
 from dataclasses import dataclass
 import enum
 import uuid
 import json
+from typing import Sequence
 from ..parseutils import parse_wcf_date
+from ..dicttools import flatten_dict
+
+
+def create_point_geo_interface(x: float, y: float) -> dict:
+    """Provided an X and Y coordinate, returns a __geo_interface__
+    Point dict.
+    """
+    if x and y:
+        coords = tuple(x, y)
+    else:
+        coords = None
+    output = {
+        "type": "Point",
+        "coordinates": coords
+    }
+    return output
+
 
 @enum.unique
 class CommercialVehicleRestrictionType(enum.IntEnum):
@@ -13,6 +32,7 @@ class CommercialVehicleRestrictionType(enum.IntEnum):
     """
     BridgeRestriction = 0
     RoadRestriction = 1
+
 
 @dataclass
 class RoadwayLocation(object):
@@ -24,6 +44,28 @@ class RoadwayLocation(object):
     MilePost: float = None
     Latitude: float = None
     Longitude: float = None
+
+    @property
+    def as_geometry(self):
+        return {
+            "type": "Point",
+            "coodrinates": tuple(self.Longitude, self.Latitude)
+        }
+
+    @property
+    def __geo_interface__(self):
+        props: dict
+        props = self.__dict__.copy()
+        x = props.pop("Longitude")
+        y = props.pop("Latitude")
+
+        return {
+            "type": "Feature",
+            "properties": {
+                "Description": props
+            },
+            "geometry": tuple(x, y)
+        }
 
 
 @dataclass
@@ -39,7 +81,19 @@ class BorderCrossingData(object):
         if self.Time and isinstance(self.Time, str):
             self.Time = parse_wcf_date(self.Time)
         if self.BorderCrossingLocation and not isinstance(self.BorderCrossingLocation, RoadwayLocation):
-            self.BorderCrossingLocation = RoadwayLocation(**self.BorderCrossingLocation)
+            self.BorderCrossingLocation = RoadwayLocation(
+                **self.BorderCrossingLocation)
+
+    @property
+    def __geo_interface__(self):
+        output: dict
+        output = self.BorderCrossingLocation.__geo_interface__
+        prop_dict: dict = output["Properties"]
+        for key in tuple("Time", "CrossingName", "WaitTime"):
+            prop_dict[key] = self.__dict__[key]
+
+        return output
+
 
 @dataclass
 class BridgeDataGIS(object):
@@ -65,6 +119,27 @@ class BridgeDataGIS(object):
     def __post_init__(self):
         if self.LocationID and not isinstance(self.LocationID, uuid.UUID):
             self.LocationID = uuid.UUID(self.LocationID)
+
+    @property
+    def __geo_interface__(self):
+        output = self.__dict__.copy()
+        x1 = output.pop("BeginLongitude")
+        y1 = output.pop("BeginLatitude")
+        x2 = output.pop("EndLongitude")
+        y2 = output.pop("EndLatitude")
+
+        geom = {}
+        geom["type"] = "MultiPoint"
+        if (x1 and y1 and x2 and y2):
+            geom["coordinates"] = tuple(tuple(x1, y1), tuple(x2, y2))
+        elif (x1 and y1):
+            geom["coordinates"] = tuple(tuple(x1, y1))
+        elif (x2 and y2):
+            geom["coordinates"] = tuple(tuple(x2, y2))
+
+        output["geometry"] = geom
+        return output
+
 
 @dataclass
 class CVRestrictionData(object):
@@ -102,11 +177,47 @@ class CVRestrictionData(object):
 
     def __post_init__(self):
         if not isinstance(self.RestrictionType, CommercialVehicleRestrictionType):
-            self.RestrictionType = CommercialVehicleRestrictionType(self.RestrictionType)
+            self.RestrictionType = CommercialVehicleRestrictionType(
+                self.RestrictionType)
         for name in ("DatePosted", "DateEffective", "DateExpires"):
             val = getattr(self, name, None)
             if isinstance(val, str):
                 setattr(self, name, parse_wcf_date(val))
+
+    @property
+    def __geo_interface__(self):
+        props = dict(flatten_dict(self.__dict__))
+        geom = {
+            "type": "Point",
+            "coordinates": tuple(map(props.pop, ("Longitude", "Latitude")))
+        }
+        return {
+            "type": "Feature",
+            "geometry": geom,
+            "properties": props
+        }
+
+
+def get_multipoint_feature(dct: dict, *xy_props: Sequence[Sequence[str]]):
+    """Converts a flattened dictionary into a multipoint feature
+    geometry interface (__geo_interface__).
+    """
+    coords = []
+    for x_key, y_key in xy_props:
+        x, y = map(dct.get, (x_key, y_key), (None,)*2)
+        if x is not None and y is not None:
+            coords.append(tuple(x, y))
+    coords = tuple(coords)
+    geo = {
+        "type": "MultiPoint",
+        "coordinates": coords
+    }
+    return {
+        "type": "Feature",
+        "properties": dct,
+        "geometry": geo
+    }
+
 
 @dataclass
 class Alert(object):
@@ -146,6 +257,14 @@ class Alert(object):
         if isinstance(self.LastUpdatedTime, str):
             self.LastUpdatedTime = parse_wcf_date(self.LastUpdatedTime)
 
+    @property
+    def __geo_interface__(self):
+        return get_multipoint_feature(self.__dict__, (
+            "StartRoadwayLocationLongitude", "StartRoadwayLocationLatitude"
+        ), (
+            "EndRoadwayLocationLongitude", "EndRoadwayLocationLatitude"
+        ))
+
 
 @dataclass
 class Camera(object):
@@ -183,6 +302,34 @@ class Camera(object):
         if self.CameraLocation is not None and not isinstance(self.CameraLocation, RoadwayLocation):
             self.CameraLocation = RoadwayLocation(**self.CameraLocation)
 
+    @property
+    def __geo_interface__(self):
+        props = copy.deepcopy(self.__dict__)
+        x: float = None
+        y: float = None
+        road_loc: RoadwayLocation = props.pop("RoadwayLocation")
+        if self.DisplayLatitude and self.DisplayLongitude:
+            x = props.pop("DisplayLongitude")
+            y = props.pop("DisplayLatitude")
+        elif self.CameraLocation.Longitude and self.CameraLocation.Latitude:
+            x = self.CameraLocation.Longitude
+            y = self.CameraLocation.Latitude
+        output = {
+            "type": "Feature",
+            "properties": props,
+            "geometry": {
+                "type": "Point",
+                "coordinates": tuple(x, y)
+            }
+        }
+
+        # Add the roadway location properties to the output properties dict
+        for key, val in road_loc.__dict__.items():
+            props[key] = val
+
+        return output
+
+
 @dataclass
 class TravelRestriction(object):
     """Describes a travel restriction
@@ -190,6 +337,7 @@ class TravelRestriction(object):
     TravelDirection: str = None
 
     RestrictionText: str = None
+
 
 @dataclass
 class PassCondition(object):
@@ -227,6 +375,24 @@ class PassCondition(object):
         if not isinstance(self.RestrictionTwo, TravelRestriction):
             self.RestrictionTwo = TravelRestriction(**self.RestrictionTwo)
 
+    @property
+    def __geo_interface__(self) -> dict:
+        props = dict(flatten_dict(self.__dict__))
+        output = {
+            "type": "Feature",
+            "properties": props
+        }
+        x = props.pop("Longitude", None)
+        y = props.pop("Latitude", None)
+        if (x and y):
+            output["geometry"] = {
+                "type": "Point",
+                "coordinates": tuple(x, y)
+            }
+        else:
+            output["geometry"] = None
+        return output
+
 
 @enum.unique
 class FlowStationReading(enum.IntEnum):
@@ -238,6 +404,7 @@ class FlowStationReading(enum.IntEnum):
     Heavy = 3
     StopAndGo = 4
     NoData = 5
+
 
 @dataclass
 class FlowData(object):
@@ -259,9 +426,23 @@ class FlowData(object):
         if isinstance(self.Time, str):
             self.Time = parse_wcf_date(self.Time)
         if not isinstance(self.FlowStationLocation, RoadwayLocation):
-            self.FlowStationLocation = RoadwayLocation(self.FlowStationLocation)
+            self.FlowStationLocation = RoadwayLocation(
+                self.FlowStationLocation)
         if not isinstance(self.FlowReadingValue, FlowStationReading):
             self.FlowReadingValue = FlowStationReading(self.FlowReadingValue)
+
+    @property
+    def __geo_interface__(self):
+        props = dict(flatten_dict(self.__dict__))
+        x, y = map(lambda name: props.pop(name, None),
+                   ("FlowStationLocationLongitude", "FlowStationLocationLatitude"))
+        geom = create_point_geo_interface(x, y)
+        return {
+            "type": "Feature",
+            "properties": props,
+            "geometry": geom
+        }
+
 
 @dataclass
 class TravelTimeRoute(object):
@@ -284,6 +465,7 @@ class TravelTimeRoute(object):
             val = getattr(self, name, None)
             if isinstance(val, str):
                 setattr(self, name, parse_wcf_date(val))
+
 
 @dataclass
 class WeatherInfo(object):
@@ -323,37 +505,53 @@ class WeatherInfo(object):
         if isinstance(self.ReadingTime, str):
             self.ReadingTime = parse_wcf_date(self.ReadingTime)
 
+    @property
+    def __geo_interface__(self):
+        props = copy.deepcopy(self.__dict__)
+        x, y = map(lambda name: props.pop, ("Longitude", "Latitude"))
+        geom = create_point_geo_interface(x, y)
+        return {
+            "type": "Feature",
+            "properties": props,
+            "geometry": geom
+        }
+
+
 @dataclass
 class TollRate(object):
     """Toll rate
     """
     SignName: str = None
-
     TripName: str = None
-
     CurrentToll: int = None
-
     CurrentMessage: str = None
-
     StateRoute: str = None
-
     TravelDirection: str = None
-
     StartMilepost: float = None
-
     StartLocationName: str = None
-
     StartLatitude: float = None
-
     StartLongitude: float = None
-
     EndMilepost: float = None
-
     EndLocationName: str = None
-
     EndLatitude: float = None
-
     EndLongitude: float = None
+
+    @property
+    def __geo_interface__(self):
+        props = copy.deepcopy(self.__dict__)
+        x1, y1, x2, y2 = map(lambda name: props.pop, (
+            "StartLongitude", "StartLatitude", "EndLongitude", "EndLatitude"))
+        geom = {
+            "type": "MultiPoint",
+            "coordinates": tuple(tuple(x1, y1), tuple(x2, y2))
+        }
+        return {
+            "type": "Feature",
+            "properties": props,
+            "geometry": geom
+        }
+
+
 
 def parse(dct: dict):
     """Specialized JSON parsing for wsdottraffic.classes classes.
@@ -381,10 +579,12 @@ def parse(dct: dict):
         return TollRate(**dct)
     return dct
 
+
 class TrafficJSONEncoder(json.JSONEncoder):
     """Encodes wsdottraffic classes back to JSON.
     Date objects are represented in ISO format strings rather than WCF date strings.
     """
+
     def default(self, obj):
         """Converts the input object into a JSON serializable object.
         """
